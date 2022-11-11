c6b01e6cb70b2968b7117c13a54c59ac44578c0f
benmackenzie <benmackenzie>
2022-11-11 15:10:15 +0000

    add notebooks to test directly in repo

---
+++ demo.py
@@ -0,0 +1,169 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC use ben_churn_model
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Let's look at the data
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from customers order by customer_id
+
+# COMMAND ----------
+
+# MAGIC %sql 
+# MAGIC select * from dbu order by customer_id, date desc
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from customer_support;
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Build a churn model
+# MAGIC ##### orient around renewal date
+# MAGIC ##### model is for customers on 3 year contracts only.
+# MAGIC ##### features: job and sql dbu growth, number of interactions with customer support, tier.
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from salesforce order by customer_id
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC create or replace view renewal_eol as select customer_id, renewal_date, commit from salesforce where contract_length =3
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from renewal_eol order by customer_id;
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Build features based on yaml spec
+
+# COMMAND ----------
+
+from features.feature_generation import build_training_data_set,feature_tables, build_feature_table, register_dimension_table, tables 
+df = build_training_data_set()
+
+# COMMAND ----------
+
+display(df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###Now lets create the corresponding feature tables
+# MAGIC ##### note that the training data set should NOT be used.   Look at feature table spec in yaml file.  Need to consider backfill, time density of features
+
+# COMMAND ----------
+
+build_feature_table(feature_tables['customer_service_calls'], drop_existing=True)
+
+# COMMAND ----------
+
+build_feature_table(feature_tables['dbu_growth'], drop_existing=True)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### handle dimension tables differently.  Just register the underlying table with feature store
+
+# COMMAND ----------
+
+register_dimension_table(tables['customers'])
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### observe that now we can build the same training data set using the feature store directly
+
+# COMMAND ----------
+
+from databricks.feature_store.client import FeatureStoreClient
+from databricks.feature_store.entities.feature_lookup import FeatureLookup
+
+fs = FeatureStoreClient()
+
+
+# COMMAND ----------
+
+feature_lookups = [
+    FeatureLookup(
+        table_name="ben_churn_model.dbu_growth",
+        feature_names=["6_month_growth_sql_dbu", "6_month_growth_job_dbu"],
+        lookup_key="customer_id",
+        timestamp_lookup_key = "renewal_date"
+    ),
+    FeatureLookup(
+        table_name="ben_churn_model.customer_service_calls",
+        feature_names=["customer_service_count"],        
+        lookup_key="customer_id",
+        timestamp_lookup_key = "renewal_date"
+    ),
+  
+   FeatureLookup(
+        table_name="ben_churn_model.customers",
+        feature_names=["tier"],        
+        lookup_key="customer_id",
+        timestamp_lookup_key = "renewal_date"
+    )
+      
+]
+renewal_eol_df = spark.sql('select * from renewal_eol')
+
+training_set = fs.create_training_set(
+    renewal_eol_df,
+    feature_lookups=feature_lookups,
+    label="commit",
+)
+training_df = training_set.load_df()
+
+# COMMAND ----------
+
+display(training_df)
+
+# COMMAND ----------
+
+display(training_df)
+
+# COMMAND ----------
+
+pip install --index-url https://test.pypi.org/simple/ --no-deps ben_mackenzie_features --upgrade
+
+
+# COMMAND ----------
+
+from features.generate import get_template_location, get_feature_sef_location
+
+#print(get_template_location())
+print(get_feature_sef_location())
+
+# COMMAND ----------
+
+import os
+os.getcwd()
+
+# COMMAND ----------
+
+pip install python-dotenv
+
+
+# COMMAND ----------
+
+from dotenv import find_dotenv, load_dotenv
+
+find_dotenv()
+
+# COMMAND ----------
+
+
---
+++ generate tables.py
@@ -0,0 +1,139 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #### use the next two cells to create and use database.  The database should correspond to the schema parameter in features.yaml
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC create database ben_churn_model
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC use ben_churn_model
+
+# COMMAND ----------
+
+from pyspark.sql.functions import explode, sequence, to_date
+
+beginDate = '2000-01-01'
+endDate = '2050-12-31'
+
+(
+  spark.sql(f"select explode(sequence(to_date('{beginDate}'), to_date('{endDate}'), interval 1 day)) as calendarDate")
+    .createOrReplaceTempView('dates')
+)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC need to add start of week, end of week, start of month, end of month to facilitate time aggs
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC create or replace table dim_calendar as
+# MAGIC select
+# MAGIC   year(calendarDate) * 10000 + month(calendarDate) * 100 + day(calendarDate) as dateInt,
+# MAGIC   calendarDate,
+# MAGIC   year(calendarDate) AS calendarYyear,
+# MAGIC   date_format(calendarDate, 'MMMM') as calendarMonth,
+# MAGIC   month(calendarDate) as MonthOfYear,
+# MAGIC   date_format(calendarDate, 'EEEE') as calendarDay,
+# MAGIC   dayofweek(calendarDate) AS dayOfWeek,
+# MAGIC   case
+# MAGIC     when weekday(calendarDate) < 5 then 'Y'
+# MAGIC     else 'N'
+# MAGIC   end as IsWeekDay,
+# MAGIC   dayofmonth(calendarDate) as dayOfMonth,
+# MAGIC   case
+# MAGIC     when calendarDate = last_day(calendarDate) then 'Y'
+# MAGIC     else 'N'
+# MAGIC   end as isLastDayOfMonth,
+# MAGIC   dayofyear(calendarDate) as dayOfYear,
+# MAGIC   weekofyear(calendarDate) as weekOfYearIso,
+# MAGIC   quarter(calendarDate) as quarterOfYear,
+# MAGIC   to_date(dateadd(day, - dayofweek(calendarDate), calendarDate)) as lastCompleteWeek,
+# MAGIC   to_date(last_day(dateadd(month, -1, calendarDate))) as lastCompleteMonth
+# MAGIC  
+# MAGIC from
+# MAGIC   dates
+# MAGIC order by
+# MAGIC   calendarDate
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT dateadd(month, -1, to_date('2009-01-12'));
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from dim_calendar;
+
+# COMMAND ----------
+
+pip install Faker
+
+# COMMAND ----------
+
+from faker import Faker
+from datetime import date, timedelta
+import random
+import pandas as pd
+
+fake = Faker()
+num_customers = 1000
+start_date = date(2019, 1, 1)
+num_days = 4 * 365 
+
+dates = [start_date + timedelta(n) for n in range(num_days)]
+customers = [fake.unique.random_int(min=100000, max=999999) for n in range(num_customers)]
+
+data = [[c, d, fake.random_int(min=100, max=10000), fake.random_int(min=100, max=10000), fake.random_int(min=100, max=10000)] for c in customers for d in dates]
+dbu_df = pd.DataFrame (data, columns = ['customer_id', 'date', 'interactive_dbu', 'job_dbu', 'sql_dbu'])
+
+customer_tier=[]
+for c in customers:
+  d = random.randint(1, num_days-2)
+ 
+  customer_tier.append([c, random.choice(['standard', 'enterprise']), dates[0], dates[d]])
+  customer_tier.append([c, random.choice(['standard', 'enterprise']), dates[d+1], date(2999,12,12)])
+  
+
+customers_df = pd.DataFrame(customer_tier, columns=['customer_id', 'tier', 'row_effective_from', 'row_expiration'])
+
+renewal = [[c, date(2022, 1,1) + timedelta(random.randint(1,180)), random.randint(1,3), random.randint(1,100) > 20] for c in customers]
+renewal_df = pd.DataFrame(renewal, columns=['customer_id', 'renewal_date', 'contract_length', 'commit'])
+
+
+customer_support = []
+for c in customers:
+  d = random.randint(1, 100)
+  while d < num_days:
+    customer_support.append([c, dates[d], random.choice(['phone', 'email', 'slack'])])
+    d += random.randint(1,100)
+
+customer_support_df = pd.DataFrame(customer_support, columns=['customer_id', 'date', 'channel'])
+
+
+
+
+
+# COMMAND ----------
+
+
+spark.createDataFrame(dbu_df).write.format('delta').mode('overwrite').option("overwriteSchema", "true").saveAsTable('dbu')
+spark.createDataFrame(customers_df).write.format('delta').mode('overwrite').option("overwriteSchema", "true").saveAsTable('customers')
+spark.createDataFrame(renewal_df).write.format('delta').mode('overwrite').option("overwriteSchema", "true").saveAsTable('salesforce')
+spark.createDataFrame(customer_support_df).write.format('delta').mode('overwrite').option("overwriteSchema", "true").saveAsTable('customer_support')
+
+
+
+# COMMAND ----------
+
+
