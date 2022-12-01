4b3b295413c0d29555230193af0ec84c34a33aed
benmackenzie <ben.mackenzie@databriks.com>
2022-12-01 11:35:40 -0500

    vscode extention

---
+++ jobs/pytest_databricks.py
@@ -0,0 +1,13 @@
+import pytest
+import os
+import sys
+
+# Run all tests in the repository root.
+
+repo_root = os.path.dirname(os.path.dirname(__file__))
+os.chdir(repo_root)
+
+# Skip writing pyc files on a readonly filesystem.
+sys.dont_write_bytecode = True
+
+_ = pytest.main(sys.argv[1:])
